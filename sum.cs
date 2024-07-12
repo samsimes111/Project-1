99583a190a54c88026dd602e6fce6b8d2fe4cf9f
Yuuu Bin <daonguyennhatanh0910@gmail.com>
2024-07-12 23:11:25 +0700

    Fix: file sum.cs

--- sum.cs
+++ sum.cs
@@ -1,8 +1,20 @@
-int a,b;
-int sum = b + a;
+namespace Tong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so thu nhat: ");
+            string input1 = Console.ReadLine();
+            int number1 = int.Parse(input1);
 
-a = 10; b =100;
+            Console.Write("Nhap so thu hai: ");
+            string input2 = Console.ReadLine();
+            int number2 = int.Parse(input2);
 
-Console.WriteLine("Tong a va b la" + sum);
+            int Tong = number1 + number2;
 
-    
+            Console.WriteLine($"Ket qua cua {number1} va {number2} la: {Tong}");
+        }
+    }
+}
