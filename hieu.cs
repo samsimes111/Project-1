3d24500da96e7ff5d6f53b890c61849c78ec44ba
Yuuu Bin <daonguyennhatanh0910@gmail.com>
2024-07-13 00:03:23 +0700

    Fix: file hieu.cs

--- hieu.cs
+++ hieu.cs
@@ -1,3 +1,22 @@
-int a,b;
-int hieu=b-a;
-    
+﻿using System;
+
+namespace Hieu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("So thu nhat: ");
+            string input1 = Console.ReadLine();
+            int number1 = int.Parse(input1);
+
+            Console.Write("So thu hai: ");
+            string input2 = Console.ReadLine();
+            int number2 = int.Parse(input2);
+
+            int hieu = number1 - number2;
+
+            Console.WriteLine($"Ket qua cua {number1} va {number2} la: {hieu}");
+        }
+    }
+}
